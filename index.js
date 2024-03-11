acbf7c60b7c0b9c11d45db94985884a66a4c03ca
VGUdawant <160871913+VGUdawant@users.noreply.github.com>
2024-03-11 21:02:20 +0530

    All files have been uploaded

---
+++ index.js
@@ -0,0 +1,140 @@
+const audio1 = new Audio("What Jhumka.mp3");
+document.querySelector(".btn1").addEventListener("click", function() {
+  audio1.play();
+});
+document.querySelector(".btnp").addEventListener("click", function() {
+  audio1.pause();
+});
+const audio2 = new Audio("chaleya.mp3");
+document.querySelector(".btn2").addEventListener("click", function() {
+  audio2.play();
+});
+document.querySelector(".btnp2").addEventListener("click", function() {
+  audio2.pause();
+});
+const audio3 = new Audio("Mast Magan.mp3");
+document.querySelector(".btn3").addEventListener("click", function() {
+  audio3.play();
+});
+document.querySelector(".btnp3").addEventListener("click", function() {
+  audio3.pause();
+});
+const audio4 = new Audio("Naina.mp3");
+document.querySelector(".btn4").addEventListener("click", function() {
+  audio4.play();
+});
+document.querySelector(".btnp4").addEventListener("click", function() {
+  audio4.pause();
+});
+const audio5 = new Audio("Teri Mitti Mein.mp3");
+document.querySelector(".btn5").addEventListener("click", function() {
+  audio5.play();
+});
+document.querySelector(".btnp5").addEventListener("click", function() {
+  audio5.pause();
+});
+const audio6 = new Audio("Tu Hi t.mp3");
+document.querySelector(".btn6").addEventListener("click", function() {
+  audio6.play();
+});
+document.querySelector(".btnp6").addEventListener("click", function() {
+  audio6.pause();
+});
+const audio7 = new Audio("Deewani Mastani.mp3");
+document.querySelector(".btn7").addEventListener("click", function() {
+  audio7.play();
+});
+document.querySelector(".btnp7").addEventListener("click", function() {
+  audio7.pause();
+});
+const audio8 = new Audio("Dholida.mp3");
+document.querySelector(".btn8").addEventListener("click", function() {
+  audio8.play();
+});
+document.querySelector(".btnp8").addEventListener("click", function() {
+  audio8.pause();
+});
+const audio9 = new Audio("Kaabil Hoon.mp3");
+document.querySelector(".btn9").addEventListener("click", function() {
+  audio9.play();
+});
+document.querySelector(".btnp9").addEventListener("click", function() {
+  audio9.pause();
+});
+const audio10 = new Audio("Labon Pe Naam.mp3");
+document.querySelector(".btn10").addEventListener("click", function() {
+  audio10.play();
+});
+document.querySelector(".btnp10").addEventListener("click", function() {
+  audio10.pause();
+});
+const audio11 = new Audio("Radha Kaise Na .mp3");
+document.querySelector(".btn11").addEventListener("click", function() {
+  audio11.play();
+});
+document.querySelector(".btnp11").addEventListener("click", function() {
+  audio11.pause();
+});
+const audio12 = new Audio("Nainowale Ne.mp3");
+document.querySelector(".btn12").addEventListener("click", function() {
+  audio12.play();
+});
+document.querySelector(".btnp12").addEventListener("click", function() {
+  audio12.pause();
+});
+const audio13 = new Audio("Deva.mp3");
+document.querySelector(".btn13").addEventListener("click", function() {
+  audio13.play();
+});
+document.querySelector(".btnp13").addEventListener("click", function() {
+  audio13.pause();
+});
+const audio14 = new Audio("Achyutm.mp3");
+document.querySelector(".btn14").addEventListener("click", function() {
+  audio14.play();
+});
+document.querySelector(".btnp14").addEventListener("click", function() {
+  audio14.pause();
+});
+const audio15 = new Audio("Sai.mp3");
+document.querySelector(".btn15").addEventListener("click", function() {
+  audio15.play();
+});
+document.querySelector(".btnp15").addEventListener("click", function() {
+  audio15.pause();
+});
+const audio16 = new Audio("Manzil.mp3");
+document.querySelector(".btn16").addEventListener("click", function() {
+  audio16.play();
+});
+document.querySelector(".btnp16").addEventListener("click", function() {
+  audio16.pause();
+});
+const audio17 = new Audio("Vitthal.mp3");
+document.querySelector(".btn17").addEventListener("click", function() {
+  audio17.play();
+});
+document.querySelector(".btnp17").addEventListener("click", function() {
+  audio17.pause();
+});
+const audio18 = new Audio("Shyama.mp3");
+document.querySelector(".btn18").addEventListener("click", function() {
+  audio18.play();
+});
+document.querySelector(".btnp18").addEventListener("click", function() {
+  audio18.pause();
+});
+const audio19 = new Audio("aai.mp3");
+document.querySelector(".btn19").addEventListener("click", function() {
+  audio19.play();
+});
+document.querySelector(".btnp19").addEventListener("click", function() {
+  audio19.pause();
+});
+const audio20 = new Audio("Hanuman Chalisa.mp3");
+document.querySelector(".btn20").addEventListener("click", function() {
+  audio20.play();
+});
+document.querySelector(".btnp20").addEventListener("click", function() {
+  audio20.pause();
+});
